b4d933f6b3665f1fd662c33c3dd128ca5efefa54
Rachele Suy <rachelesuy@gmail.com>
2019-04-11 17:35:01 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,29 @@
+# the methiode will take a argument of the board array and return the number of turns that have played
+board =[" ", " ", " ", " ", " ", " ", " ", " "," "]
+def turn_count(board)
+  counter = 0 
+  #move_turn = []
+  board.each do |element| 
+    if element == "X" || element == "O"
+     
+      puts " element correspondant is #{element}"
+      puts "the position occupied is #{counter}"
+       counter += 1
+    end
+  end
+ return counter
+ 
+  
+end
+
+#this methode will should take in an  argument of the game board and use the turn_count to determinate if is X or turn O
+
+def current_player(board)
+  current_pla = turn_count(board)
+  if current_pla %2==0
+    return "X"
+  else
+    return "O"
+  end
+  
+end
